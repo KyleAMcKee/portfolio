4923fd3d9670317f78ce7af09f4b5af2fe331184
Kyle Mckee <kyleandrewmckee@gmail.com>
2019-04-27 09:58:59 -0700

    add main sections

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const header = () => {
-
+    return `<header>Top</header>`
 }
 
 const footer = () => {
-    
+    return `<footer>This is at the bottom</footer>`
 }
 
-const page = () => {
+const mainPage = () => {
     return `<h1>This is a site</h1>`
 }
 
-document.querySelector('body').innerHTML = page();
+const renderPage = () => {
+    return `<div class=page>
+                ${header()}
+                ${mainPage()}
+                ${footer()}
+            </div>`
+
+}
+
+document.querySelector('body').innerHTML = renderPage();
