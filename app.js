cd23ab8a4253c25906d4be2931bfa6b9440a23a4
Kyle <kyleandrewmckee@gmail.com>
2019-04-27 12:23:36 -0700

    main layout set up, skills images added

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const header = () => {
-    return `<header>Top</header>`
+    return `<header>
+                <nav class="menu">
+                    <ul>
+                        <li id="home">Home</li>
+                        <li id="projects">Projects</li>
+                        <li id="skills">Skills</li>
+                        <li id="resume">Resume</li>
+                    </ul>
+                </nav>
+            </header>`
 }
 
 const footer = () => {
@@ -7,7 +16,100 @@ const footer = () => {
 }
 
 const mainPage = () => {
-    return `<h1>This is a site</h1>`
+    return `<div class=page-content>
+                ${home()}
+            </div>`
+}
+
+const renderSkill = (skill) => {
+    return `<div class=grid-box>
+                <div class=img-container>
+                    <img src="public/${skill}.png">
+                </div>
+                <p>${skill}</p>
+            </div>`
+}
+
+const devOps = () => {
+    let devOps = ['Github', 'Git', 'Webpack', 'Vim'];
+    return `<div class=skill-wrapper>
+                <h2>Dev Ops</h2>
+                <div class=grid-wrapper>
+                    ${devOps.map(technology => renderSkill(technology)).join('')}
+                </div>
+            </div>`
+}
+
+const web = () => {
+    let web = ['HTML5', 'CSS3', 'NodeJS', 'React'];
+    return `<div class=skill-wrapper>
+                <h2>Web</h2>
+                <div class=grid-wrapper>
+                    ${web.map(technology => renderSkill(technology)).join('')}
+                </div>
+            </div>`
+}
+
+const languages = () => {
+    let languages = ['C', 'C++', 'Python', 'Javascript'];
+    return `<div class=skill-wrapper>
+                <h2>Languages</h2>
+                <div class=grid-wrapper>
+                    ${languages.map(language => renderSkill(language)).join('')}
+                </div>
+            </div>`
+}
+
+const database = () => {
+    let database = ['PostgreSQL', 'MongoDB', 'MySQL'];
+    return `<div class=skill-wrapper>
+                <h2>Database</h2>
+                <div class=grid-wrapper>
+                    ${database.map(technology => renderSkill(technology)).join('')}
+                </div>
+            </div>`
+}
+
+const renderProjects = () => {
+    
+}
+
+const home = () => {
+    return `<div>
+                <h2>You are Home</h2>
+            </div>`
+}
+
+const skills = () => {
+    return `<div class="skills">
+                ${languages()}
+                ${web()}
+                ${database()}
+                ${devOps()}
+            </div>`
+}
+
+const projects = () => { 
+    return `<div class="projects>
+                ${renderProjects()}
+            </div>`
+}
+
+const resume = () => {
+    return `<div>
+                <h2>Resume</h2>
+            </div>`
+}
+
+const renderSelection = (selection) => {
+    document.querySelector('.page-content').innerHTML = selection();
+}
+
+const setEventListeners = () => {
+    document.querySelector('#home').addEventListener('click', () => renderSelection(home));
+    document.querySelector('#projects').addEventListener('click', () => renderSelection(projects));
+    document.querySelector('#skills').addEventListener('click', () => renderSelection(skills));
+    document.querySelector('#resume').addEventListener('click', () => renderSelection(resume));
 }
 
 const renderPage = () => {
@@ -20,3 +122,4 @@ const renderPage = () => {
 }
 
 document.querySelector('body').innerHTML = renderPage();
+setEventListeners();
