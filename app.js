f01179247e81d5c4b67f4141d75ea51dbc0ce900
Kyle Mckee <kmckee@e1z3r5p2.42.us.org>
2019-04-27 09:52:06 -0700

    initial page structure

---
+++ app.js
@@ -0,0 +1,13 @@
+const header = () => {
+
+}
+
+const footer = () => {
+    
+}
+
+const page = () => {
+    return `<h1>This is a site</h1>`
+}
+
+document.querySelector('body').innerHTML = page();
